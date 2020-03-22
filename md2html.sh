903b2c30cd6cea0c4aeacfa072a005223022337b
B Ruml <b@ruml.com>
2020-03-22 15:58:30 -0400

    initial commit

---
+++ md2html.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# see repo for markdown-to-html: https://github.com/cwjohan/markdown-to-html
+# Note also: pandoc
+
+echo "Starting conversion"
+markdown $1.md --template template.html > $1.html
+echo "Finished conversion"
+# cat blogtop.html $1.html blogbottom.html > temp
+# mv temp $1.html
+cp $1.html /Library/WebServer/Documents/rumlcomrepo/coronavirus/
+echo "Copied html file to rumlcomrepo/coronavirus/"
+cp $1.html /Library/WebServer/Documents/rumlcomrepo/coronavirus/index.html
+echo "Copied html file to rumlcomrepo/coronavirus/index.html"
+echo "Done"
